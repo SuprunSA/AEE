34fc18cb187a0e2315b36f9ddd8d74806cbc6a00
Супрун Сергей <4everyongg@mail.ru>
2021-11-05 03:09:41 +0400

    Add project files.

---
+++ AlreadyExistsException/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AEE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> values = new Dictionary<string, int>();
+
+            var count = 0;
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Введите значение >>>");
+                    var value = Console.ReadLine().ToLower().Trim();
+                    if (values.ContainsKey(value))
+                    {
+                        throw new AlreadyExistsException(value, values[value]);
+                    }
+                    values.Add(value, ++count);
+                }
+            }
+            catch(AlreadyExistsException e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("{0} - значение, {1} - номер вхождения значения", e.Value, e.Position);
+            }
+        }
+    }
+
+    class AlreadyExistsException : ArgumentException
+    {
+        public string Value { get; set; }
+        public int Position { get; set; }
+
+        public AlreadyExistsException(string value, int pos) : base("Такое значение уже было введено!")
+        {
+            Value = value;
+            Position = pos;
+        }
+    }
+}
